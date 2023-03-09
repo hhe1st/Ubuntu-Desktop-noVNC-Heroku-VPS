e09222371c024352602b79a8629d066ee2a784ac
hhe1st <123032462+hhe1st@users.noreply.github.com>
2023-03-08 22:26:12 -0500

    Update supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-set -ex
-exec supervisord -c /system/supervisord.conf
+
+/usr/bin/supervisord -c /system/conf.d/supervisord.conf &
+
+cd /usr/share/novnc/utils
+./launch.sh --vnc localhost:5900 &
+cd /
+
+export DISPLAY=:0
+Xvfb :0 -screen 0 1024x768x16 &
+sleep 5
+x11vnc -display :0 -noxdamage -wait 5 -shared -nopw -forever &
+xfce4-session &
+dbus-launch --exit-with-session gnome-session &
+
+# Keep the script running to prevent container exit
+tail -f /dev/null
